3b1bc71504d729841f4494653e195b4a7207ff34
Eduardo Ávila <ea618369@gmail.com>
2025-08-29 08:45:25 -0300

    .

--- detalhes_produto.php
+++ detalhes_produto.php
@@ -83,7 +83,6 @@ $produto = null;
 
   <footer class="bg-primary p-4 mt-auto"></footer>
 
-  <!-- JS do Bootstrap e jQuery -->
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" xintegrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
